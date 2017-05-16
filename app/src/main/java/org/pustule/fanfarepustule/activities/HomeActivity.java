64a563aedf41fbd22775fbc10b7cc2ccac67c4fe
Paul Mougin <paul.mougin@gmx.com>
2017-05-16 16:12:00 +0200

    Add Butterkniffe

--- app/src/main/java/org/pustule/fanfarepustule/activities/HomeActivity.java
+++ app/src/main/java/org/pustule/fanfarepustule/activities/HomeActivity.java
@@ -1,15 +1,12 @@
 package org.pustule.fanfarepustule.activities;
 
-import android.os.Bundle;
-
 import org.pustule.fanfarepustule.R;
 import org.pustule.fanfarepustule.base.BaseActivity;
 
 public class HomeActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    protected int getLayoutRes() {
+        return R.layout.activity_home;
     }
 }
--- app/src/main/java/org/pustule/fanfarepustule/base/BaseActivity.java
+++ app/src/main/java/org/pustule/fanfarepustule/base/BaseActivity.java
@@ -1,15 +1,30 @@
 package org.pustule.fanfarepustule.base;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by Paul Mougin on 11/05/2017.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutRes());
+        ButterKnife.bind(this);
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutRes();
 
     @Override
     protected void attachBaseContext(Context newBase) {

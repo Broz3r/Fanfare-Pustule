8782d7384d833ece6b68d109bf61a3377e27ebc8
Paul Mougin <paul.mougin@gmx.com>
2017-05-16 16:26:40 +0200

    Use Loading Activity in front of Home

--- app/src/main/java/org/pustule/fanfarepustule/activities/LoadingActivity.java
+++ app/src/main/java/org/pustule/fanfarepustule/activities/LoadingActivity.java
@@ -1,8 +1,37 @@
 package org.pustule.fanfarepustule.activities;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import org.pustule.fanfarepustule.R;
+import org.pustule.fanfarepustule.base.BaseActivity;
+import org.pustule.fanfarepustule.utils.NavigationUtils;
+
 /**
  * Created by Paul Mougin on 16/05/2017.
  */
 
-public class LoadingActivity {
+public class LoadingActivity extends BaseActivity {
+
+    private static final int DELAY = 2000;
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_loading;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                NavigationUtils.launchHomeActivity(LoadingActivity.this);
+                finish();
+            }
+        }, DELAY);
+    }
 }

da02aaeea98583ed2da18e21d2879fc98a18bcc8
Paul Mougin <paul.mougin@gmx.com>
2017-05-18 15:14:14 +0200

    Add GoogleCalendarExempleActivity

--- app/src/main/java/org/pustule/fanfarepustule/activities/HomeActivity.java
+++ app/src/main/java/org/pustule/fanfarepustule/activities/HomeActivity.java
@@ -1,12 +1,22 @@
 package org.pustule.fanfarepustule.activities;
 
+import android.content.Intent;
+
 import org.pustule.fanfarepustule.R;
 import org.pustule.fanfarepustule.base.BaseActivity;
+
+import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
 
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_home;
+    }
+
+    @OnClick(R.id.sheets_button)
+    public void onSheetsButtonCLicked() {
+        final Intent intent = new Intent(this, GoogleCalendarExempleActivity.class);
+        startActivity(intent);
     }
 }
